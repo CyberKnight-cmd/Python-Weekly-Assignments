fdbb1eb8a0a44793d3ef38d4afd50a586f766769
Arya Gupta <aryagupta164@gmail.com>
2024-11-27 21:14:54 +0530

    Completed 22

--- Assignment22/Prog3.py
+++ Assignment22/Prog3.py
@@ -1,5 +1,13 @@
 # Assignment22
 # Prog 3
-# Write your question here(in shortened form)
-
+"""
+55555  
+4444 
+333 
+22  
+1
+"""
 # Write your code here
+
+n = int(input("Enter N = "))
+print("\n".join([f"{n-i+1}"*(n-i+1) for i in range(1,n+1)]))
