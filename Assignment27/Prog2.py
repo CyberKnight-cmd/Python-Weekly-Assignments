8ec56f09a8d35101a83ba52a5965e001643fd824
Arya Gupta <aryagupta164@gmail.com>
2024-11-28 01:03:02 +0530

    Completed 27

--- Assignment27/Prog2.py
+++ Assignment27/Prog2.py
@@ -1,5 +1,12 @@
 # Assignment27
 # Prog 2
-# Write your question here(in shortened form)
+#Write a Python program to calculate the factorial using user defined function. 
 
 # Write your code here
+def fact(a):
+    if a<2:
+        return 1
+    else:
+        return a*fact(a-1)
+    
+print(fact(int(input("Enter a number : "))))
--- Assignment27/Prog3.py
+++ Assignment27/Prog3.py
@@ -1,5 +1,10 @@
 # Assignment27
 # Prog 3
-# Write your question here(in shortened form)
+# Write a Python program to calculate a to the power b (a^b) using user defined function without using pow ()
 
 # Write your code here
+def power(a,b):
+    return a**b
+
+a,b = map(int, input("Enter the base and the power : ").split())
+print(power(a,b))
